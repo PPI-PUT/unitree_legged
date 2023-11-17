5bf2c5b993d5c8b9ee087265e3ba890e51c25250
Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>
2023-11-17 22:17:20 +0100

    fix: remove gui state publisher
    Signed-off-by: Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>

--- a1_description/launch/view_a1.launch.py
+++ a1_description/launch/view_a1.launch.py
@@ -64,7 +64,7 @@ def generate_launch_description():
     )
 
     nodes_to_start = [
-        joint_state_publisher_node,
+        # joint_state_publisher_node,
         robot_state_publisher_node,
         rviz_node,
     ]
