fc7b06268a723be4ed7710dc0769c62a780b10a1
Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>
2023-12-19 17:49:02 +0100

    feat: add functions
    Signed-off-by: Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>

--- unitree_a1_joystick/include/unitree_a1_joystick/unitree_a1_joystick_node.hpp
+++ unitree_a1_joystick/include/unitree_a1_joystick/unitree_a1_joystick_node.hpp
@@ -17,43 +17,51 @@
 #define UNITREE_A1_JOYSTICK__UNITREE_A1_JOYSTICK_NODE_HPP_
 
 #include "unitree_a1_joystick/unitree_a1_joystick.hpp"
+#include "unitree_a1_legged_msgs/srv/gait.hpp"
+#include "unitree_a1_legged_msgs/msg/controller_type.hpp"
 #include <geometry_msgs/msg/twist_stamped.hpp>
-
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
 
 double calcMappingdouble(const double input, const double sensitivity)
 {
-    const double exponent = 1.0 / (std::max(0.001, std::min(1.0, sensitivity)));
-    return std::pow(input, exponent);
+  const double exponent = 1.0 / (std::max(0.001, std::min(1.0, sensitivity)));
+  if (input < 0.0) {
+    return -std::pow(input, exponent);
+  }
+  return std::pow(input, exponent);
 }
 
 namespace unitree_a1_legged
 {
-    class UNITREE_A1_JOYSTICK_PUBLIC UnitreeJoystickNode : public rclcpp::Node
-    {
-    public:
-        explicit UnitreeJoystickNode(const rclcpp::NodeOptions &options = rclcpp::NodeOptions());
+class UNITREE_A1_JOYSTICK_PUBLIC UnitreeJoystickNode : public rclcpp::Node
+{
+public:
+  explicit UnitreeJoystickNode(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
 
-    private:
-        double update_rate_;
-        double linear_ratio_;
-        double angular_ratio_;
-        double linear_x_sensitivity_;
-        double linear_y_sensitivity_;
-        double linear_velocity_limit_;
-        double angular_z_sensitivity_;
-        double angular_velocity_limit_;
-        rclcpp::TimerBase::SharedPtr timer_;
-        rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_publisher_;
-        rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joystick_subscriber_;
-        rclcpp::Time last_joy_received_time_;
-        std::shared_ptr<UnitreeJoystick> joy_;
-        void receiveJoystickCallback(const sensor_msgs::msg::Joy::SharedPtr msg);
-        void publishTwist();
-        void timerCallback();
-        bool isDataReady();
-    };
+private:
+  double update_rate_;
+  double linear_ratio_;
+  double angular_ratio_;
+  double linear_x_sensitivity_;
+  double linear_y_sensitivity_;
+  double linear_velocity_limit_;
+  double angular_z_sensitivity_;
+  double angular_velocity_limit_;
+  rclcpp::Duration button_hold_duration_ = rclcpp::Duration(0, 0);
+  rclcpp::Time last_time_button_pressed_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_publisher_;
+  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joystick_subscriber_;
+  rclcpp::Time last_joy_received_time_;
+  std::shared_ptr<UnitreeJoystick> joy_;
+  rclcpp::Client<unitree_a1_legged_msgs::srv::Gait>::SharedPtr client_gait_;
+  void receiveJoystickCallback(const sensor_msgs::msg::Joy::SharedPtr msg);
+  void publishTwist();
+  void indexButtonCallback();
+  void timerCallback();
+  bool isDataReady();
+};
 } // namespace unitree_a1_legged
 
 #endif // UNITREE_A1_JOYSTICK__UNITREE_A1_JOYSTICK_NODE_HPP_
--- unitree_a1_joystick/src/unitree_a1_joystick_node.cpp
+++ unitree_a1_joystick/src/unitree_a1_joystick_node.cpp
@@ -17,86 +17,112 @@
 
 namespace unitree_a1_legged
 {
-  UnitreeJoystickNode::UnitreeJoystickNode(const rclcpp::NodeOptions &options)
-      : Node("unitree_a1_joystick", options)
-  {
-    // Parameters
-    update_rate_ = this->declare_parameter<int>("update_rate", 50);
-    linear_ratio_ = this->declare_parameter<double>("linear_ratio", 0.5);
-    angular_ratio_ = this->declare_parameter<double>("angular_ratio", 0.5);
-    linear_x_sensitivity_ = this->declare_parameter<double>("linear_x_sensitivity", 0.5);
-    linear_y_sensitivity_ = this->declare_parameter<double>("linear_y_sensitivity", 0.5);
-    linear_velocity_limit_ = this->declare_parameter<double>("linear_velocity_limit", 0.5);
-    angular_z_sensitivity_ = this->declare_parameter<double>("angular_z_sensitivity", 0.5);
-    angular_velocity_limit_ = this->declare_parameter<double>("angular_velocity_limit", 0.5);
-    // Create timer
-    const auto period_ns =
-        std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::duration<double>(1.0 / update_rate_));
-    timer_ = rclcpp::create_timer(
-        this, get_clock(), period_ns, std::bind(&UnitreeJoystickNode::timerCallback, this));
+UnitreeJoystickNode::UnitreeJoystickNode(const rclcpp::NodeOptions & options)
+: Node("unitree_a1_joystick", options)
+{
+  // Parameters
+  update_rate_ = this->declare_parameter<int>("update_rate", 50);
+  linear_ratio_ = this->declare_parameter<double>("linear_ratio", 0.5);
+  angular_ratio_ = this->declare_parameter<double>("angular_ratio", 0.5);
+  linear_x_sensitivity_ = this->declare_parameter<double>("linear_x_sensitivity", 0.5);
+  linear_y_sensitivity_ = this->declare_parameter<double>("linear_y_sensitivity", 0.5);
+  linear_velocity_limit_ = this->declare_parameter<double>("linear_velocity_limit", 0.5);
+  angular_z_sensitivity_ = this->declare_parameter<double>("angular_z_sensitivity", 0.5);
+  angular_velocity_limit_ = this->declare_parameter<double>("angular_velocity_limit", 0.5);
+  // Create timer
+  const auto period_ns =
+    std::chrono::duration_cast<std::chrono::nanoseconds>(
+    std::chrono::duration<double>(
+      1.0 /
+      update_rate_));
+  timer_ = rclcpp::create_timer(
+    this, get_clock(), period_ns, std::bind(&UnitreeJoystickNode::timerCallback, this));
 
-    // Create publishers
-    twist_publisher_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("~/output/cmd_vel", 1);
-    // Create subscribers
-    joystick_subscriber_ = this->create_subscription<sensor_msgs::msg::Joy>("~/input/joy", 1, std::bind(&UnitreeJoystickNode::receiveJoystickCallback, this, std::placeholders::_1));
-  }
+  // Create publishers
+  twist_publisher_ =
+    this->create_publisher<geometry_msgs::msg::TwistStamped>("~/output/cmd_vel", 1);
+  // Create subscribers
+  joystick_subscriber_ = this->create_subscription<sensor_msgs::msg::Joy>(
+    "~/input/joy", 1, std::bind(
+      &UnitreeJoystickNode::receiveJoystickCallback, this,
+      std::placeholders::_1));
+}
 
-  void UnitreeJoystickNode::receiveJoystickCallback(const sensor_msgs::msg::Joy::SharedPtr msg)
-  {
-    last_joy_received_time_ = msg->header.stamp;
-    joy_ = std::make_shared<UnitreeJoystick>(*msg);
-  }
+void UnitreeJoystickNode::receiveJoystickCallback(const sensor_msgs::msg::Joy::SharedPtr msg)
+{
+  last_joy_received_time_ = msg->header.stamp;
+  joy_ = std::make_shared<UnitreeJoystick>(*msg);
+}
 
-  void UnitreeJoystickNode::publishTwist()
-  {
-    auto twist_msg = geometry_msgs::msg::TwistStamped();
-    twist_msg.header.stamp = this->now();
-    if (joy_->linear_x())
-    {
-      twist_msg.twist.linear.x =
-          linear_ratio_ * calcMappingdouble(static_cast<double>(joy_->linear_x()), linear_x_sensitivity_);
-    }
-    if (joy_->linear_y())
-    {
-      twist_msg.twist.linear.y =
-          linear_ratio_ * calcMappingdouble(static_cast<double>(joy_->linear_y()), linear_y_sensitivity_);
-    }
-    if (joy_->angular_z())
-    {
-      twist_msg.twist.angular.z =
-          angular_ratio_ * calcMappingdouble(static_cast<double>(joy_->angular_z()), angular_z_sensitivity_);
-    }
-    twist_publisher_->publish(twist_msg);
+void UnitreeJoystickNode::publishTwist()
+{
+  auto twist_msg = geometry_msgs::msg::TwistStamped();
+  twist_msg.header.stamp = this->now();
+  if (joy_->linear_x()) {
+    twist_msg.twist.linear.x =
+      linear_ratio_ *
+      calcMappingdouble(static_cast<double>(joy_->linear_x()), linear_x_sensitivity_);
   }
+  if (joy_->linear_y()) {
+    twist_msg.twist.linear.y =
+      linear_ratio_ *
+      calcMappingdouble(static_cast<double>(joy_->linear_y()), linear_y_sensitivity_);
+  }
+  if (joy_->angular_z()) {
+    twist_msg.twist.angular.z =
+      angular_ratio_ * calcMappingdouble(
+      static_cast<double>(joy_->angular_z()), angular_z_sensitivity_);
+  }
+  twist_publisher_->publish(twist_msg);
+}
 
-  void UnitreeJoystickNode::timerCallback()
-  {
-    if (!isDataReady())
-    {
-      return;
-    }
-    publishTwist();
-  }
+void UnitreeJoystickNode::indexButtonCallback()
+{
+  rclcpp::Time now = this->get_clock()->now();
 
-  bool UnitreeJoystickNode::isDataReady()
-  {
-    if (!joy_)
-    {
-      RCLCPP_WARN_THROTTLE(
-          get_logger(), *get_clock(), std::chrono::milliseconds(5000).count(),
-          "waiting for joy msg...");
-      return false;
-    }
-    constexpr auto timeout = 2.0;
-    const auto time_diff = this->now() - last_joy_received_time_;
-    if (time_diff.seconds() > timeout)
-    {
-      RCLCPP_WARN_THROTTLE(
-          get_logger(), *get_clock(), std::chrono::milliseconds(5000).count(), "joy msg is timeout");
-      return false;
-    }
-    return true;
+  if (joy_->stand()) {
+    RCLCPP_INFO(get_logger(), "going Stand");
+    button_hold_duration_ = button_hold_duration_ + (now - last_time_button_pressed_);
+  } else {
+    button_hold_duration_ = rclcpp::Duration(0, 0);
   }
+  if (button_hold_duration_ > rclcpp::Duration(1, 0) ) {
+    RCLCPP_INFO(get_logger(), "Stand");
+    auto request = std::make_shared<unitree_a1_legged_msgs::srv::Gait::Request>();
+    auto msg = unitree_a1_legged_msgs::msg::ControllerType();
+    msg.type = 1;
+    request->method = msg; //unitree_a1_legged_msgs::msg::ControllerType::FIXED_STAND;
+    client_gait_->async_send_request(request);
+    button_hold_duration_ = rclcpp::Duration(0, 0);
+  }
+  last_time_button_pressed_ = now;
+}
+void UnitreeJoystickNode::timerCallback()
+{
+  if (!isDataReady()) {
+    return;
+  }
+  publishTwist();
+  indexButtonCallback();
+}
+
+bool UnitreeJoystickNode::isDataReady()
+{
+  if (!joy_) {
+    RCLCPP_WARN_THROTTLE(
+      get_logger(), *get_clock(), std::chrono::milliseconds(5000).count(),
+      "waiting for joy msg...");
+    return false;
+  }
+  constexpr auto timeout = 2.0;
+  const auto time_diff = this->now() - last_joy_received_time_;
+  if (time_diff.seconds() > timeout) {
+    RCLCPP_WARN_THROTTLE(
+      get_logger(), *get_clock(), std::chrono::milliseconds(5000).count(), "joy msg is timeout");
+    return false;
+  }
+  return true;
+}
 } // namespace unitree_a1_legged
 
 #include "rclcpp_components/register_node_macro.hpp"
